c854bc416f47584e9826dd874c699a5931357860
1Taafe <dima759mi@gmail.com>
2022-11-17 08:34:45 +0300

    Initial commit

---
+++ src/test/java/page/BaseClothesPage.java
@@ -0,0 +1,30 @@
+package page;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+
+public class BaseClothesPage {
+
+    private final By jeansLocator = By.xpath("//button[text()='Джинсы']");
+    final static int WAIT_TIME_IN_SECONDS = 15;
+    WebDriver driver;
+
+    public BaseClothesPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public boolean openJeansPage(){
+        WebElement iframe = new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIME_IN_SECONDS))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("iFrameResizer0")));
+        driver.switchTo().frame(iframe);
+        System.out.println(driver.getPageSource());
+        WebElement jeansButton = new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIME_IN_SECONDS))
+               .until(ExpectedConditions.presenceOfElementLocated(jeansLocator));
+        jeansButton.click();
+        return true;
+    }
+
+}
