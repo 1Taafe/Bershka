64550b931687a7ef315d1b7fa6738099caa62700
1Taafe <dima759mi@gmail.com>
2022-11-17 21:42:24 +0300

    test

--- src/test/java/test/BerskaTest.java
+++ src/test/java/test/BerskaTest.java
@@ -17,9 +17,7 @@ public class BerskaTest {
     @BeforeMethod
 
     public void InitializeDriver(){
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("--headless");
-        driver = new FirefoxDriver(options);
+        driver = new FirefoxDriver();
     }
 
     @Test
