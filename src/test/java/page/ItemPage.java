729f32c0b25517f3fa97048eb11fa3712ea588ae
1Taafe <dima759mi@gmail.com>
2022-12-02 08:46:32 +0300

    only test10

--- src/test/java/page/ItemPage.java
+++ src/test/java/page/ItemPage.java
@@ -51,17 +51,9 @@ public class ItemPage {
     }
 
     public void choose45Size(){
-        try{
-            WebElement size45Button = (new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIME_IN_SECONDS)))
-                    .until(ExpectedConditions.elementToBeClickable(size45Locator));
-            size45Button.click();
-        }
-        catch (Exception ex){
-            WebElement size45Button = (new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIME_IN_SECONDS)))
-                    .until(ExpectedConditions.elementToBeClickable(size45Locator));
-            size45Button.click();
-        }
-
+        WebElement size45Button = (new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIME_IN_SECONDS)))
+                .until(ExpectedConditions.elementToBeClickable(size45Locator));
+        size45Button.click();
     }
 
     public void chooseLSize(){
